91e2114f4ace55b377d2f1bed9feb2c895bd3900
Ruboki <ruboki.reuchi@gmail.com>
2023-02-13 21:05:38 +0100

    create terrain with sprite shape

---
+++ Assets/Scripts/Entities/Player/Demon_Aura.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demon_Aura : MonoBehaviour
+{
+    public void UnactiveDemonParticles()
+    {
+        gameObject.SetActive(false);
+    }
+}
